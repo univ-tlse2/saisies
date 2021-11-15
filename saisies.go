63a59d6a3498698d9499497de7c573929ccc80c8
jaco60 <jacoboni@univ-tlse2.fr>
2021-11-15 08:07:42 +0100

    Add init function to avoid multiple scanner openings

--- saisies.go
+++ saisies.go
@@ -6,9 +6,14 @@ import (
 	"os"
 )
 
+var scanner *bufio.Scanner
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+}
+
 // Input affiche un message et renvoie la chaîne saisie par l'utilisateur
 func Input(mess string) string {
-	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print(mess)
 	scanner.Scan()
 	return scanner.Text()
